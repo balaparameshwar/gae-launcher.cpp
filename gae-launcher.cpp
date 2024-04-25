7e6f86f3e02c575692af087a4d8afda51f76dd7d
Balaparameshwar Reddy Rondla <160993046+balaparameshwar@users.noreply.github.com>
2024-04-25 14:03:21 +0530

    Create gae-launcher.cpp

---
+++ gae-launcher.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    cout<<"Balaparameshwar"<<endl;
+    cout<<"21BCS5544"<<endl;
+    cout<<"21BCS-CC-620-A"<<endl;
+    cout<<"mark = 28/30?"<<endl;
+    return 0;
+}
